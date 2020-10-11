4e675c040c57fc7f59f09e446791f172cdfb024b
Rounak Vyas <itsron143@gmail.com>
2020-10-11 18:05:46 +0530

    added ssl cert; fixed links

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const Index = () => (
     <p>In my free time, I enjoy playing table tennis, backpacking once in a while and Indo-Chinese food.</p>
     <p>You can find me on <XA href="https://twitter.com/itsron143">Twitter</XA>, <XA href="https://github.com/itsron717">Github</XA>,<XA href="https://www.linkedin.com/in/itsron143/"> LinkedIn</XA>, and <XA href="https://www.strava.com/athletes/57025908">Strava</XA>, or reach me via <XA href="mailto:rounakvyas@outlook.com">email</XA>.</p>
     <h2>Colophon</h2>
-    <p>The credits for this excellet template goes to <XA href="https://twitter.com/prestonrichey">Preston Richey</XA>. This site was made with <XA href="https://www.gatsbyjs.org/">Gatsby.js</XA> and is hosted on <XA href="https://www.netlify.com/">Netlify</XA>. You can look at its code <XA href="https://github.com/itsron717">here</XA>. The monospace font is <XA href="https://fonts.google.com/specimen/Roboto+Mono">Roboto Mono</XA> and the sans-serif is <XA href="https://fonts.google.com/specimen/Rubik">Rubik</XA>.</p>
+    <p>The credits for this excellet template goes to <XA href="https://twitter.com/prestonrichey">Preston Richey</XA>. This site was made with <XA href="https://www.gatsbyjs.org/">Gatsby.js</XA> and is hosted on <XA href="https://www.netlify.com/">Netlify</XA>. You can look at its code <XA href="https://github.com/itsron717/rounakvyas.me">here</XA>. The monospace font is <XA href="https://fonts.google.com/specimen/Roboto+Mono">Roboto Mono</XA> and the sans-serif is <XA href="https://fonts.google.com/specimen/Rubik">Rubik</XA>.</p>
   </Layout>
 );
 
